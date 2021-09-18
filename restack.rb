1ba28a59b2c011b8ceb5ee79123e68ad6ed77de9
Abhinav Gupta <41730+abhinav@users.noreply.github.com>
2021-09-18 14:53:52 -0700

    restack: Update formula to v0.5.1

--- restack.rb
+++ restack.rb
@@ -5,29 +5,29 @@
 class Restack < Formula
   desc "Makes interactive Git rebase nicer."
   homepage "https://github.com/abhinav/restack"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/abhinav/restack/releases/download/v0.5.0/restack_0.5.0_Darwin_x86_64.tar.gz"
-      sha256 "52fdd3253ba15b65c6a7eb63cebf6de9e4f06341527e2aab0f5d5e64c0752773"
+      url "https://github.com/abhinav/restack/releases/download/v0.5.1/restack_0.5.1_Darwin_x86_64.tar.gz"
+      sha256 "b69e42c824970a4b1d5120763bd0ce322bd0817af2336b10199884da5863928c"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/abhinav/restack/releases/download/v0.5.0/restack_0.5.0_Darwin_arm64.tar.gz"
-      sha256 "6b52c122487f205f4747a93b697542069ee9324020adea9059f4a86854262a27"
+      url "https://github.com/abhinav/restack/releases/download/v0.5.1/restack_0.5.1_Darwin_arm64.tar.gz"
+      sha256 "be1ae57497f4181afc0c44a661d508b6ed3d8942f93400d3c3e1a1a2bf0edd00"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/abhinav/restack/releases/download/v0.5.0/restack_0.5.0_Linux_x86_64.tar.gz"
-      sha256 "3185d9cc93f8306855ddbc4657d8297291b24ab36a223cc12a45e74b1208e7a3"
+      url "https://github.com/abhinav/restack/releases/download/v0.5.1/restack_0.5.1_Linux_x86_64.tar.gz"
+      sha256 "26588a1d9b2e2c5611af89ac0864f31d8a159aa31698e4a582e4a27809e1c427"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/abhinav/restack/releases/download/v0.5.0/restack_0.5.0_Linux_arm64.tar.gz"
-      sha256 "099649bb607a03831d58a3a7308bca4f8617a98d0cb76785ec7dc7f48bf8b174"
+      url "https://github.com/abhinav/restack/releases/download/v0.5.1/restack_0.5.1_Linux_arm64.tar.gz"
+      sha256 "6089698fd68ff03d192514d55ad004be93650f0484771bd52ffd6c5cb144a2a9"
     end
   end
 
